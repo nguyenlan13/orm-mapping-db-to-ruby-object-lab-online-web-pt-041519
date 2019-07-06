1a0992e24e7fcf6bd2359168ff344cfd504be076
Lan Nguyen <nguyenlan13@gmail.com>
2019-07-06 07:50:29 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -4,6 +4,7 @@ class Student
   def self.new_from_db(row)
     # create a new Student object given a row from the database
     student = Student.new(row)
+    DB[:conn].execute(sql)
   end
 
   def self.all
