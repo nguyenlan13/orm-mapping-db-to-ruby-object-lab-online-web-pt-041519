e72e32ad9f0ebdc85499c1eb18767f61dec96d04
Lan Nguyen <nguyenlan13@gmail.com>
2019-07-06 08:42:00 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -97,7 +97,7 @@ class Student
    def self.first_student_in_grade_10
     sql = <<-SQL
       SELECT COUNT(*)
-      from students
+      FROM students
       WHERE grade = 10
       ORDER BY student.id
       LIMIT 1;
