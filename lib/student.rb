c8b5403168da408c9971c5b33d03f6391d6b65d1
Lan Nguyen <nguyenlan13@gmail.com>
2019-07-06 08:49:37 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -94,6 +94,17 @@ class Student
     end
   end
   
+    def self.students_below_12th_grade
+    sql = <<-SQL
+      SELECT COUNT(*)
+      from students
+      WHERE grade < 12;
+    SQL
+    DB[:conn].execute(sql).map do |row|
+      self.new_from_db(row)
+    end
+  end
+  
    def self.first_student_in_grade_10
     sql = <<-SQL
       SELECT *
